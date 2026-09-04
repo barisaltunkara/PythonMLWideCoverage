7f996038dcb9e7f41e2f914c8abbd252b5bbd18b
Barış Altunkara <baris.altunkara@gmail.com>
2026-09-04 19:26:37 +0300

    R2evaluation.py addition

---
+++ R2evaluation.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  4 17:41:22 2026
+
+@author: Barış
+"""
+
+"""R2 - R-Square Yöntemi
+Hata Kareleri Toplamı = Topla(yi - y'i)^2
+Ortalama Farkların Toplamı = Topla(yi - yort)^2
+R^2 = 1 - HKT/OFT
+"""
+
+
+
+
+
+
+
+
+
