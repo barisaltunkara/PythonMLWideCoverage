f644758d17e1d4db988cbbab0dc0b56656022966
Barış Altunkara <baris.altunkara@gmail.com>
2026-09-16 19:50:12 +0300

    Update naivebayes.py

--- Classification/naivebayes.py
+++ Classification/naivebayes.py
@@ -5,7 +5,38 @@ Created on Mon Sep 14 19:19:45 2026
 @author: Barış
 """
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn import preprocessing
+from sklearn.metrics import confusion_matrix
+from sklearn.naive_bayes import GaussianNB
 
+veriler = pd.read_csv("../data/veriler.csv")
+
+x = veriler.iloc[:, 1:4].values
+y = veriler.iloc[:, 4:].values
+
+le = preprocessing.LabelEncoder()
+
+y = le.fit_transform(y.ravel())
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=1)
+
+sc = StandardScaler()
+
+X_train = sc.fit_transform(x_train) 
+X_test = sc.transform(x_test)
+
+gnb = GaussianNB()
+gnb.fit(X_train, y_train)
+
+y_pred_gnb = gnb.predict(X_test)
+
+cm = confusion_matrix(y_test, y_pred_gnb)
+print(cm)
 
 
 
